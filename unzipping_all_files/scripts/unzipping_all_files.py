f6ca3d4bbc148fb55234818300cd87446ed932bf
João Manoel Feck <joaomfeck@gmail.com>
2021-01-21 14:22:52 -0300

    Add files via upload
    App to unzip multiple .zip files with one click.
    Just place the .zip files on the data_incoming folder and execute the batch. After execution, the output will be on the data_outgoing folder

---
+++ unzipping_all_files/scripts/unzipping_all_files.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 15:38:26 2019
+
+@author: WCY8676
+"""
+
+import zipfile
+import os 
+
+data_incoming_foldername = 'data_incoming'
+data_outgoing_foldername = 'data_outgoing'
+
+path_script = os.getcwd()
+path_project = os.path.dirname(path_script)
+path_incoming = os.path.join(path_project, data_incoming_foldername)
+path_outgoing = os.path.join(path_project, data_outgoing_foldername)
+
+
+# initializing empty file paths list 
+file_paths = [] 
+  
+# crawling through directory and subdirectories 
+for root, directories, files in os.walk(path_incoming): 
+    for filename in files: 
+        # join the two strings in order to form the full filepath. 
+        file_with_no_ext = filename.split('.')[0]
+        inc_file_path = os.path.join(root, filename) 
+        file_paths.append([inc_file_path, filename]) 
+        
+for file in file_paths:
+    new_unzip = zipfile.ZipFile(file[0], "r")
+    new_unzip.extractall(path_outgoing)
+    new_unzip.close()
+    
